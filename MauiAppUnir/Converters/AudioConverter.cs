1d52bdf06a3e69ff57d7399b49d207a26e18381f
RafaelQuintero <RafaelQuintero@DESKTOP-D9F79Q8>
2024-02-22 15:47:55 -0700

    Agregar archivos de proyecto.

---
+++ MauiAppUnir/Converters/AudioConverter.cs
@@ -0,0 +1,26 @@
+﻿
+
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+
+namespace MauiAppUnir.Converters;
+
+
+public class AudioConverter
+{
+    public static MemoryStream ConvertMp3ToWav(string mp3File)
+    {
+
+        using var fileStream = File.OpenRead(mp3File);
+        using var wavStream = new MemoryStream();
+
+        using var reader = new WaveFileReader(fileStream);
+        var resampler = new WdlResamplingSampleProvider(reader.ToSampleProvider(), 16000);
+        WaveFileWriter.WriteWavFileToStream(wavStream, resampler.ToWaveProvider16());
+
+        // This section sets the wavStream to the beginning of the stream. (This is required because the wavStream was written to in the previous section)
+        wavStream.Seek(0, SeekOrigin.Begin);
+        return wavStream;
+    }
+}
---
+++ MauiAppUnir/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppUnir
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
